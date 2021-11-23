016f747d37377bdded0b6ee24ffbb6bfe277f6a3
Sylwester Biega <biegson11@gmail.com>
2021-11-23 14:11:02 +0100

    innit commit

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,7 @@
-<template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
-</template>
-
+<template></template>
+<script>
+export default {};
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
